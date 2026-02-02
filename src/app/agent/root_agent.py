75eba112c6f768482f59be957b893f1ec172d9b8
Ruben civeira <rubenciveira@gmail.com>
2026-02-02 14:06:21 +0100

    feat: chain of thought

--- src/app/agent/root_agent.py
+++ src/app/agent/root_agent.py
@@ -5,7 +5,7 @@ import time
 from langchain_ollama import OllamaLLM
 from app.config import AppConfig
 from app.rag.project_context import ProjectContextRetriever
-from app.context.thinking_step import ThinkingStep, ThinkingResult
+from app.context.thinking_step import ThinkingStep, ThinkingResult, AnstractThinkingStep
 
 class RootAgent:
     def __init__(self, config: AppConfig):
@@ -18,45 +18,94 @@ class RootAgent:
 
     def execute(self, msg: str) -> ThinkingStep:
         if msg == "hola":
-            f5 = ThinkingStep("fak5", lambda: self._trap("f5" + msg))
-            f4 = ThinkingStep("fak4", lambda: self._trap("f4" + msg), next = lambda _: self._next(f5))
-            f3 = ThinkingStep("fak3", lambda: self._trap("f3" + msg), next = lambda _: self._next(f4))
-            f2 = ThinkingStep("fak2", lambda: self._trap("f2" + msg), next = lambda _: self._next(f3))
-            f1 = ThinkingStep("fak1", lambda: self._trap("f1" + msg), next = lambda _: self._next(f2))
-            return f1
+            return FakeStep(msg, 1)
+            # f5 = ThinkingStep("fak5", lambda: self._trap("f5" + msg))
+            # f4 = ThinkingStep("fak4", lambda: self._trap("f4" + msg), next = lambda _: self._next(f5))
+            # f3 = ThinkingStep("fak3", lambda: self._trap("f3" + msg), next = lambda _: self._next(f4))
+            # f2 = ThinkingStep("fak2", lambda: self._trap("f2" + msg), next = lambda _: self._next(f3))
+            # f1 = ThinkingStep("fak1", lambda: self._trap("f1" + msg), next = lambda _: self._next(f2))
+            # return f1
         if msg.startswith("En contexto"):
-            return ThinkingStep("lookup rag", 
-                lambda: self._lookup_lambda(msg),
-                next = lambda prev: self._ask_context(prev, msg) 
-            )
-        return ThinkingStep("thinking", lambda: self._run(msg))
+            return RagStep(msg, self)
+            # return ThinkingStep("lookup rag", 
+            #     lambda: self._lookup_lambda(msg),
+            #     next = lambda prev: self._ask_context(prev, msg) 
+            # )
+        return CallStep(msg, self)
+        # return ThinkingStep("thinking", lambda: self._run(msg))
     
-    def _next(self, th: ThinkingStep) -> ThinkingStep:
-        return th
+#     def _next(self, th: ThinkingStep) -> ThinkingStep:
+#         return th
 
-    def _trap(self, msg: str) -> str:
+#     def _trap(self, msg: str) -> str:
+#         time.sleep(2)
+#         return msg
+
+#     def _ask_context(self, prev: ThinkingResult, msg: string) -> str:
+#         time.sleep(2)
+#         context = prev.context or prev.response
+#         return ThinkingStep("thinking", lambda: self._run("""
+# Usa SOLO el contexto para responder.
+
+# Contexto:
+# {context}
+
+# Pregunta: {msg}
+# """))
+
+#     def _lookup_lambda(self, msg: str) -> str:
+#         time.sleep(2)
+#         rag = ProjectContextRetriever(self.config)
+#         ctx = rag.get_active_context( msg )
+#         # return "EL CONTEXTO ES " + ctx
+#         return ThinkingResult("Tengo context " + str(len(ctx)), ctx)
+
+#     def _run(self, msg: str) -> str:
+#         return self.llm.invoke( msg )
+    
+
+class FakeStep(AnstractThinkingStep):
+    def __init__(self, msg, index: int):
+        super().__init__("fake " + str(index))
+        self.msg = msg
+        self.index = index
+
+    def think(self):
         time.sleep(2)
-        return msg
+        return "F" + str(self.index) + self.msg
 
-    def _ask_context(self, prev: ThinkingResult, msg: string) -> str:
+    def and_then(self, prev):
+        if self.index < 5:
+            return FakeStep(self.msg, self.index + 1)
+
+class RagStep(AnstractThinkingStep):
+    def __init__(self, msg: str, root: RootAgent):
+        super().__init__("rag")
+        self.msg = msg
+        self.agent = root
+    def think(self):
+        time.sleep(2)
+        rag = ProjectContextRetriever(self.agent.config)
+        ctx = rag.get_active_context( self.msg )
+        # return "EL CONTEXTO ES " + ctx
+        return ThinkingResult("Tengo context " + str(len(ctx)), ctx)
+    def and_then(self, prev):
         time.sleep(2)
         context = prev.context or prev.response
-        return ThinkingStep("thinking", lambda: self._run("""
+        return CallStep("""
 Usa SOLO el contexto para responder.
 
 Contexto:
 {context}
 
 Pregunta: {msg}
-"""))
+""", self.agent)
 
-    def _lookup_lambda(self, msg: str) -> str:
-        time.sleep(2)
-        rag = ProjectContextRetriever(self.config)
-        ctx = rag.get_active_context( msg )
-        # return "EL CONTEXTO ES " + ctx
-        return ThinkingResult("Tengo context " + str(len(ctx)), ctx)
+class CallStep(AnstractThinkingStep):
+    def __init__(self, msg: str, root: RootAgent):
+        super().__init__("thinkg")
+        self.msg = msg
+        self.agent = root
+    def think(self):
+        return self.agent.llm.invoke( self.msg )
 
-    def _run(self, msg: str) -> str:
-        return self.llm.invoke( msg )
-    
--- src/app/context/thinking_step.py
+++ src/app/context/thinking_step.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 from dataclasses import dataclass
 from typing import Callable, Optional
+from abc import ABC, abstractmethod
 
 @dataclass(frozen=True)
 class ThinkingStep:
@@ -13,3 +14,23 @@ class ThinkingStep:
 class ThinkingResult:
     response: str
     context: Optional[str] = None
+
+class AnstractThinkingStep(ABC):
+    def __init__(self, action: str):
+        self.action = action
+        self.invoke = self.think
+        self.next = lambda r: self.and_then(r)
+
+    @abstractmethod
+    def think(self) -> ThinkingResult | str:
+        pass
+
+    def and_then(self, prev: ThinkingResult) -> ThinkingStep | None:
+        pass
+
+    # def build(self) -> ThinkingStep:
+    #     return ThinkingStep(
+    #         action=self.__class__.__name__,
+    #         invoke=self.think,
+    #         next=lambda r: self.and_then(r).build() if self.and_then(r) else None
+    #     )
